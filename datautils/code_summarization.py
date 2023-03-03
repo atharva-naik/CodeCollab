eefc8675e8b3faa1abf64ce12f67c838bb1c2103
Atharva Naik <atharvanaik2018@gmail.com>
2023-03-03 09:14:17 -0500

    pushing code summarization

--- datautils/code_summarization.py
+++ datautils/code_summarization.py
@@ -8,7 +8,7 @@ class CodeSummarizer:
         self.tokenizer = RobertaTokenizer.from_pretrained(model_path)
         self.model = T5ForConditionalGeneration.from_pretrained(model_path)
 
-    def __call__(self, text: str, max_length: int=10):
+    def __call__(self, text: str, max_length: int=40):
         # text = "def greet(user): print(f'hello <extra_id_0>!')"
         input_ids = self.tokenizer(text, return_tensors="pt").input_ids
         # simply generate a single sequence
@@ -23,7 +23,8 @@ class CodeSummarizer:
 if __name__ == "__main__":
     code_summarizer = CodeSummarizer()
     val_data = read_jsonl("./data/juice-dataset/dev.jsonl")
-    code = val_data[0]["code"]
+    code = "def greet(user): print(f'hello <extra_id_0>!')"
+    # val_data[0]["code"]
     intent = code_summarizer(code)
     print(code)
     print(intent)
