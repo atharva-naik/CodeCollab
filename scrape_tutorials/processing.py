555bb57dc62df5019304beb61636ab952f7a8e66
Atharva Naik <atharvanaik2018@gmail.com>
2023-03-22 00:36:02 -0400

    adding KG paths for sklearn

--- scrape_tutorials/processing.py
+++ scrape_tutorials/processing.py
@@ -66,7 +66,7 @@ class KGPathsIndex:
 if __name__ == "__main__":
     # target_module = "numpy" # "pandas_toms_blog" # "seaborn"
     os.makedirs("./scrape_tutorials/KG_paths", exist_ok=True)
-    module_list = ["scipy"] #["numpy", "pandas_toms_blog", "seaborn", "torch", "scipy"]
+    module_list = ["sklearn"] #["numpy", "pandas_toms_blog", "seaborn", "torch", "scipy", "sklearn"]
     for target_module in tqdm(module_list):
         kg_paths = KGPathsIndex(f"./scrape_tutorials/KGs/{target_module}.json")
         kg_paths_save_path = f"./scrape_tutorials/KG_paths/{target_module}.json"
